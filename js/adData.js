5e41c382fae8c98fe035f006489636cb37b42ced
ElenaSolovyeva <elena.solo.mail2014@gmail.com>
2020-06-25 20:24:55 +0200

    Пытается исправить замечание по вызову загрузки данных

--- js/adData.js
+++ js/adData.js
@@ -7,42 +7,48 @@ window.adData = (function () {
   var xhr = new XMLHttpRequest();
   xhr.responseType = 'json';
   xhr.timeout = TIMEOUT;
-  var adList = [];
 
-  var onLoad = function (data) {
-    for (var i = 0; i < data.length; i += 1) {
-      adList.push(data[i]);
-    }
+  var loadAdData = function () {
+    var adList = [];
+
+    var onLoad = function (data) {
+      for (var i = 0; i < data.length; i += 1) {
+        adList.push(data[i]);
+      }
+    };
+
+    var onError = function (message) {
+      var errorBlock = document.createElement('p');
+      errorBlock.innerHTML = '<p style="color: red">' + message + '</p>';
+      errorBlock.style.width = '100%';
+      document.querySelector('.map__title').insertAdjacentHTML('beforeend', errorBlock.innerHTML);
+    };
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === STATUS_OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Объявления других пользователей не найдены.   ' + 'Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.open('GET', URL);
+
+    xhr.send();
+
+    console.log(adList);
+
     return adList;
   };
 
-  var onError = function (message) {
-    var errorBlock = document.createElement('p');
-    errorBlock.innerHTML = '<p style="color: red">' + message + '</p>';
-    errorBlock.style.width = '100%';
-    document.querySelector('.map__title').insertAdjacentHTML('beforeend', errorBlock.innerHTML);
-  };
 
-  xhr.addEventListener('load', function () {
-    if (xhr.status === STATUS_OK) {
-      onLoad(xhr.response);
-    } else {
-      onError('Объявления других пользователей не найдены.   ' + 'Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
-    }
-  });
-
-  xhr.addEventListener('error', function () {
-    onError('Произошла ошибка соединения');
-  });
-
-  xhr.addEventListener('timeout', function () {
-    onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-  });
-
-  xhr.open('GET', URL);
-
-  xhr.send();
-
-
-  return {adList: adList};
+  return {loadAdData: loadAdData};
 })();
